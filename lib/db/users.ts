31ec5862a736391f37f285524932bbabda31d64f
NithinKonda <nithinkonda142@gmail.com>
2025-01-18 14:05:16 +0530

    add user and user progress interfaces; implement functions to retrieve all users and user progress

--- lib/db/users.ts
+++ lib/db/users.ts
@@ -1,4 +1,21 @@
 import clientPromise from "@/lib/mongodb";
+import { ObjectId } from "mongodb";
+
+export interface User {
+  _id: string;
+  email: string;
+  role: string;
+  name?: string;
+  image?: string;
+}
+
+export interface UserProgress {
+  userId: string;
+  courseId: string;
+  completedMaterials: string[];
+  lastAccessedAt: Date;
+  completedAt?: Date;
+}
 
 export async function getUserCount() {
   const client = await clientPromise;
@@ -10,6 +27,20 @@ export async function getUser(id: string) {
   const client = await clientPromise;
   const db = client.db();
   return await db.collection("users").findOne({ _id: id });
+}
+
+export async function getAllUsers() {
+  const client = await clientPromise;
+  const db = client.db();
+  return await db.collection("users").find().toArray();
+}
+
+export async function getUserProgress(userId: string) {
+  const client = await clientPromise;
+  const db = client.db();
+  return await db.collection("courseProgress")
+    .find({ userId })
+    .toArray();
 }
 
 export async function updateUserRole(userId: string, role: string) {
